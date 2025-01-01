8d64749665223852cb9d25994bb06633b4dad1a8
Serchinastico <github@serchinastico.com>
2025-01-01 21:07:27 +0100

    Day 01: Vertical or horizontal lines only.

--- src/day01.ts
+++ src/day01.ts
@@ -3,7 +3,13 @@ const CONFIG = {
   HALF_HEIGHT: window.innerHeight * 0.5,
   WIDTH: window.innerWidth,
   HEIGHT: window.innerHeight,
+  NUMBER_OF_LINES: 100,
+  CANVAS_WIDTH: window.innerWidth * 0.5,
+  CANVAS_HEIGHT: window.innerWidth * 0.15,
+  LINE_WIDTH: 2,
 };
+
+const LINE_GAP = CONFIG.CANVAS_WIDTH / CONFIG.NUMBER_OF_LINES;
 
 interface CanvasAnimation {
   frame: number;
@@ -19,18 +25,31 @@ const prepareCanvas = ({ id }: { id: string }) => {
 const render = ({ animation, context }: { animation: CanvasAnimation; context: CanvasRenderingContext2D }) => {
   context.clearRect(0, 0, CONFIG.WIDTH, CONFIG.HEIGHT);
 
-  context.lineWidth = 1;
+  context.lineWidth = CONFIG.LINE_WIDTH;
   context.strokeStyle = "#FA0F22";
 
   context.save();
-  context.translate(CONFIG.HALF_WIDTH, CONFIG.HALF_HEIGHT);
-  context.beginPath();
-  context.arc(0, 0, 100, 0, 2 * Math.PI);
-  context.stroke();
+  context.translate(CONFIG.HALF_WIDTH - CONFIG.CANVAS_WIDTH / 2, CONFIG.HALF_HEIGHT);
+
+  for (let i = 0; i < CONFIG.NUMBER_OF_LINES; i++) {
+    const x = i * LINE_GAP;
+
+    const yComponent =
+      0.01 * CONFIG.CANVAS_HEIGHT * Math.sin(0.5 * (animation.frame + x)) +
+      0.14 * CONFIG.CANVAS_HEIGHT * Math.sin(0.1 * (-animation.frame + x)) +
+      0.85 * CONFIG.CANVAS_HEIGHT * Math.sin(0.01 * (animation.frame + x));
+    context.lineWidth = 2 + 0.01 * (CONFIG.CANVAS_HEIGHT - Math.abs(yComponent)) * CONFIG.LINE_WIDTH;
+
+    context.beginPath();
+    context.moveTo(x, 0);
+    context.lineTo(x, yComponent);
+    context.stroke();
+    context.closePath();
+  }
+
   context.restore();
 
   animation.frame += 1;
-
   requestAnimationFrame(() => render({ animation, context }));
 };
 
